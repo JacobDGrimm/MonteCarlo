216d1402c7112dc63e5dc52c1a93a37274845a5b
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-09-13 19:18:34 -0400

    Add project files.

---
+++ MonteCarlo/PiBaby.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarlo
+{
+    class PiBaby
+    {
+        Random rnd = new Random();
+
+        public double Hipotanus()
+        {
+            double[] coord = new double[2];
+
+            for (int i = 0; i < coord.Length; i++)
+            {
+                coord[i] = rnd.NextDouble();
+            }
+
+            double hipot = 
+        }
+    }
+}
---
+++ MonteCarlo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("How big would you like the array to be?");
+            double[] arr = new double[Convert.ToInt32(Console.ReadLine())];
+            double inCircle = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var PB = new PiBaby();
+                arr[i] = PB.Hipotanus;
+                if (arr[i] <= 1.0)
+                {
+                    inCircle++;
+                }
+            }
+            double piAproximation = (inCircle / arr.Length) * 4.0;
+            Console.WriteLine($"The number of values inside the circle were {inCircle},\n" +
+                $"The number calculated to pi for you is {}\n" +
+                $"The difference between real pi and your pi is: {Math.Abs(Math.PI - piAproximation)}");
+        }
+    }
+}
