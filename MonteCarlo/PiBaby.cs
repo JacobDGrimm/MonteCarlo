3d285c340b403b085b5a0f5608f422537226d9df
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-09-13 21:19:18 -0400

    Fisishes the monte calro method

--- MonteCarlo/PiBaby.cs
+++ MonteCarlo/PiBaby.cs
@@ -8,17 +8,15 @@ namespace MonteCarlo
 {
     class PiBaby
     {
+
         
 
-        public double Hipotanus()
-        {
-            double[] coord = new double[2];
 
-            for (int i = 0; i < coord.Length; i++)
-            {
-                Random rnd = new Random();
-                coord[i] = rnd.NextDouble();
-            }
+        public double Hipotanus(Random rnd)
+        {
+            double[] coord = new double[] { rnd.NextDouble(), rnd.NextDouble() };
+
+            
 
             double hipot = (Math.Sqrt(Math.Pow(coord[0], 2) + Math.Pow(coord[1], 2)));
             return hipot;
--- MonteCarlo/Program.cs
+++ MonteCarlo/Program.cs
@@ -8,16 +8,17 @@ namespace MonteCarlo
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            
+            Random rnd = new Random();
             Console.WriteLine("How big would you like the array to be?");
             double[] arr = new double[Convert.ToInt32(Console.ReadLine())];
             double inCircle = 0;
             for (int i = 0; i < arr.Length; i++)
             {
                 var PB = new PiBaby();
-                arr[i] = PB.Hipotanus();
+                arr[i] = PB.Hipotanus(rnd);
                 if (arr[i] <= 1.0)
                 {
                     inCircle++;
@@ -25,8 +26,9 @@ namespace MonteCarlo
             }
             double piAproximation = (inCircle / arr.Length) * 4.0;
             Console.WriteLine($"The number of values inside the circle were {inCircle},\n" +
-                $"The number calculated to pi for you is {piAproximation}\n" +
+                $"The number calculated as pi for you is {piAproximation}\n" +
                 $"The difference between real pi and your pi is: {Math.Abs(Math.PI - piAproximation)}");
+            Console.ReadLine();
         }
     }
 }
